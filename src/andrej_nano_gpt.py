0a2fd5a3cd8b0e1b2dbb65b9111cec3c92c1f445
boyko <boyko.todorov@mantiumai.com>
2025-09-16 17:48:47 -0400

    Added my parrot of Andrej Karpathy's nano-gpt

--- src/andrej_nano_gpt.py
+++ src/andrej_nano_gpt.py
@@ -5,17 +5,17 @@ from torch import nn
 from torch.functional import F
 
 # hyperparameters
-batch_size = 4  # how many independent sequences will we process in parallel?
-block_size = 8  # what is the maximum context length for predictions?
+batch_size = 64  # how many independent sequences will we process in parallel?
+block_size = 256  # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
-n_head = 4
+n_embed = 384
+n_head = 6
 head_size = n_embed // n_head
-n_layer = 3
+n_layer = 6
 dropout = 0.2
 # ------------
 
